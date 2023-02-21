f6380fee34f039dab8a40acb4d9fc5015b1cf073
anaky94 <anaky.44@gmail.com>
2023-02-21 21:44:33 +0100

    1erExo

---
+++ terre00.py
@@ -0,0 +1,8 @@
+#créer un programme qui affiche l'alphabet en lettre minuscules suivi d'un retoure à la ligne.
+
+alphabet = ["a b c d e f g h i j k l m n o p q r s t u v w x y z" ]
+for lettre in alphabet:
+    print()
+    print (lettre) 
+    
+   
