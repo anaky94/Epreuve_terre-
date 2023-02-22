eeb4d56782ea8cd12da529d87126e7b37a1b1190
anaky94 <anaky.44@gmail.com>
2023-02-22 04:06:11 +0100

    terre04

--- terre02.py
+++ terre02.py
@@ -1,1 +1,7 @@
-#crée un argument
+#crée un argument qui affiche les arguments qu'il reçoit ligne par ligne, peutimporte le nombre d'argument 
+
+argument = ["je", "suis", "solide", "et", "beau", "!"]
+for argur in argument:
+    print(argur)
+
+
---
+++ terre03.py
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+alphabet = ["n o p q r s t u v w x y z" ]
+for lettre in alphabet:
+    print()
+    print (lettre) 
