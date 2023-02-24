a71fa586eff8b7c8a16d295524e8f70a23ae7ed1
anaky94 <anaky.44@gmail.com>
2023-02-24 03:44:26 +0100

    epreuve O4

--- terre04.py
+++ terre04.py
@@ -32,47 +32,21 @@ for lettre in apire:
 """
 #determiner un bout de code qui donne les entiers au hasard
 #determiner que pair et impair apparaisse 
-#faire en sorte qu'il donne un entier (paire ou impai)
+#faire en sorte qu'il donne un entier (paire ou impair)
 
-#input("quel est l'entier? ")
-#i = int(input("Entrez un chiffre "))
-
-#if i % int(i) == 0:
- #   print("pair")
-#else:
-#    print("impair")
+i = int(input("Entrez un chiffre "))
+a=2
+if a % i == 0 :
+    print("pair")
+else:
+    print("impair")
 
 """ d= durée en min 
 ecrire un programe en python  pour convertir une duré en minute entré par l'utilisateur en heure minute (nimbre de minute par l'utilisateur est un nombre entier) 
-exomple 120 le programe doit affiché 2h00
+exemple 120 le programe doit affiché 2h00
 // quotien 
 % modulo seront utiliser 
 """
-i=int(input("entrez les mins "))
-h= i //60
-m= i%60
-print(h, m)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 """d=int(input("entrez les mins "))
