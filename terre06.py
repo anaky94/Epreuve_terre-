22534e3b03fd6903b2330da221d6e434758753c0
anaky94 <anaky.44@gmail.com>
2023-02-28 20:20:02 +0100

    07

--- terre06.py
+++ terre06.py
@@ -1,11 +1,4 @@
 #Créer un programe qui affiche l'inverse de la chaine de caractère donée en argument 
 
-# 1 demander a l'utilisateur d'écrire sa chaine de caractère 
-
-#  2 etablir un programe qui reçoit les chaines de caractère en entré 
-# 3 faire en sorte qu'en sortie les chaines de caractère rentré par l'utilisateur soit inversé
-
-c=input("entré des chaine de caractères :")
-d=c
-print(c)
-
+a=input("entré un mot ")
+print(a [:: -1])
