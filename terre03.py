072ccc6555a51504530d95e6e372622089b28bce
anaky94 <anaky.44@gmail.com>
2023-02-28 08:58:29 +0100

    perdu

--- terre03.py
+++ terre03.py
@@ -1,7 +1,7 @@
 #Créez un programme qui affiche l’alphabet à partir de la lettre donnée en argument en lettres minuscules suivi d’un retour à la ligne
 
 
-
+input("ecrit l'alphabet commençant par n ")
 alphabet = ["n o p q r s t u v w x y z" ]
 for lettre in alphabet:
     print (lettre) 
--- terre06.py
+++ terre06.py
@@ -5,5 +5,5 @@
 #  2 etablir un programe qui reçoit les chaines de caractère en entré 
 # 3 faire en sorte qu'en sortie les chaines de caractère rentré par l'utilisateur soit inversé
 
-#c=input("entré des chaine de caractères :")
+c=input("entré des chaine de caractères :")
 
