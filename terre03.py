74f39228801d5522ba83a47909b863b46a297baa
anaky94 <anaky.44@gmail.com>
2023-03-08 07:40:21 +0100

    exo

--- terre03.py
+++ terre03.py
@@ -1,9 +1,17 @@
 #Créez un programme qui affiche l’alphabet à partir de la lettre donnée en argument en lettres minuscules suivi d’un retour à la ligne
 
-
+"""
 alphabet=input("entre une letre :")
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 for lettre in range(26):
     
     print (alphabet=alphabet+str(1)) 
-    input()
+    input()"""
+
+alphabet = []
+lettre=input("entter yu letter ")
+start = ord(lettre)
+for i in range (26):
+    alphabet.append(chr(start+i))
+
+print(alphabet)
