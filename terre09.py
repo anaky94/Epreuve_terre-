98c191d56d60ab26810a9c2855e4496cf15c874c
anaky94 <anaky.44@gmail.com>
2023-03-12 13:18:40 +0100

    exo

--- terre09.py
+++ terre09.py
@@ -1,7 +1,18 @@
 
 #créez un programme qui affiche la racine carée d'un entier positif 
 
+"""a=int(input("calculer la racine carré de : "))
+b=0.5
+print(pow(a,b))"""
 
+import math
 
-
-
+a = int(input("calculer la racine caré de : "))
+try:
+    a=print(math.sqrt(a))
+    if a <=1:
+        print("erreur: entrez un entier positif")
+    
+except:
+    print("ValueError: entré un entier positif")
+    
