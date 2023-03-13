d11df08af83fa17e62eb6ca2716c0dcbee65fd6c
anaky94 <anaky.44@gmail.com>
2023-03-13 11:10:02 +0100

    exoe

--- terre11.py
+++ terre11.py
@@ -1,4 +1,7 @@
 # Créez un programe qui transforme une heure affiché en format 24h en une heure affichée en format 12h
-import datetime
-date=datetime.datetime.now()
-print(date)
+d=str(input("indiquez le format d'heure en 24h:"))
+c=d(-12)
+if d >= 12:
+    print(c,":pm")
+elif d <=12:
+    print(c,"0 :am")
