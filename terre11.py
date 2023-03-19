a359bd2278eaa452f098b2d07f03b33b73836da9
anaky94 <anaky.44@gmail.com>
2023-03-19 20:56:40 +0100

    exo

--- terre11.py
+++ terre11.py
@@ -6,6 +6,19 @@ if d >= 12:
 elif d <=12:
     print(c,"0 :am")"""
 
-from datetime import datetime
+def convert(s):
+    
+      if s[-2:] == "AM" and s[:2] == "12":
+         return "00" + s[2:-2]
 
-d=datetime.strftime("6:58","%H:%M")
+      elif s[-2:] == "AM":
+         return s[:-2]
+
+      elif s[-2:] == "PM" and s[:2] == "12":
+         return s[:-2]
+        
+      else:
+          return str(int(s[:2]) + 12) + s[2:8]
+
+s=input("entre l'heure ")
+print(convert(s))
