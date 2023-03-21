392d5b8d1e6342513a674cf352b058e94e815322
anaky94 <anaky.44@gmail.com>
2023-03-21 08:18:48 +0100

    exo

--- terre11.py
+++ terre11.py
@@ -8,17 +8,17 @@ elif d <=12:
 
 def convert(s):
     
-      if s[-2:] == "AM" and s[:2] == "12":
+      if s[-2:] == "AM" and s[:2] == "-12":
          return "00" + s[2:-2]
 
       elif s[-2:] == "AM":
          return s[:-2]
 
-      elif s[-2:] == "PM" and s[:2] == "12":
+      elif s[-2:] == "PM" and s[:2] == "-12":
          return s[:-2]
         
       else:
-          return str(int(s[:2]) + 12) + s[2:8]
+          return str(int(s[:2]) - 12) + s[2:8]
 
 s=input("entre l'heure ")
 print(convert(s))
--- terre12.py
+++ terre12.py
@@ -1,1 +1,23 @@
 #Créez un programme qui transforme une heure affichée en format 12h en une heure affichée en format 24h.
+
+
+def convert(s):
+    
+      if s[-2:] == "AM" and s[:2] == "12":
+         return "00" + s[2:-2]
+
+      elif s[-2:] == "AM":
+         return s[:-2]
+
+      elif s[-2:] == "PM" and s[:2] == "12":
+         return s[:-2]
+        
+      else:
+          return str(int(s[:2]) + 12) + s[2:8]
+
+s=input("entre l'heure ")
+print(convert(s))
+
+
+
+
