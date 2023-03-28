0fe9c42fc6e4379192c6ed11ac72354ed22ffd5f
anaky94 <anaky.44@gmail.com>
2023-03-28 08:30:49 +0200

    exo

--- terre13.py
+++ terre13.py
@@ -1,23 +1,27 @@
 #Créez un programme qui prend en paramètre trois entiers et affiche la valeur du milieu.
 
-#exemple 9 8 3 => pas trié
+#exemple 11 40 34 => 34
 
 # etape 1: inscrit un parametre qui prend 3 entier => fonction input
 
-#etape 2: les triés si il se suivent du genre 1,2,3 alors il sont trié si non 1,3,5 alors il ne sont pas trié
-
+# etape 2: les triés si il se suivent du genre 1,2,3 alors il sont trié si non 1,3,5 alors il ne sont pas trié
+# calculer la moyen des 3 trois element afficher 
+# 
 #etape 3:
 
 
 
 
-b=int(input("entré un entier"))
-a=int(input("entré un entier"))
-c=int(input("entré un entier"))
+b=int(input("entré un entier "))
+a=int(input("entré un entier "))
+c=int(input("entré un entier "))
 
-if b%a and b % c:
-    print("trié")
-else :
+ma_liste=[b, a, c]
+element=max(ma_liste) #affiche l'element le plus grand 
+print(element)
+#mo=a,b,c/3
+
+print(/3)
 
 
 
