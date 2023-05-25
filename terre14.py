b89d22621197045e8540b5c5cd1ca0c0aec0c249
anaky94 <anaky.44@gmail.com>
2023-05-26 00:16:12 +0200

    epreuve terre 14

--- terre14.py
+++ terre14.py
@@ -1,1 +1,23 @@
 #Créez un programme qui détermine si une liste d’entiers est triée ou pas
+"""
+$> ruby exo.rb 9 8 3
+Pas triée !
+
+$> ruby exo.rb 3 8 9 12
+Triée !
+
+$> ruby exo.rb “Salut”
+erreur.
+"""
+tri=int(input("taper un chifre"))
+tri2=int(input("taper un chifre"))
+tri3=int(input("taper un chifre"))
+
+tri, tri2, tir3=nombre_int
+if tri <= tri2 and tri:
+    print("triée")
+elif tri2 <=tri3:
+    print("triée")
+else:
+    print("pas trié")
+
